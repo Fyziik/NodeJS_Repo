e6e3c2da38bc0d913139e54b94d108db6e6980e5
Andreas <Fyziikme@gmail.com>
2021-02-04 14:17:30 +0100

    completed 'arrays'

--- 0._Misc/1._Intro_Exercises/4._arrays.js
+++ 0._Misc/1._Intro_Exercises/4._arrays.js
@@ -5,7 +5,7 @@
 
 const letters = ["a","b","c"];
 // show b in the console 
-
+console.log(letters[1])
 
 // --------------------------------------
 // Exercise 2 - Array Positioning
@@ -13,7 +13,22 @@ const letters = ["a","b","c"];
 const friends = [];
 
 // What a lonely array. Add at least 3 friend objects to it.  
+friends.push({
+    "name": "Andreas",
+    "age": 25
+})
 
+friends.push({
+    "name": "Ida",
+    "age": 17
+})
+
+friends.push({
+    "name": "Ole",
+    "age": 65
+})
+
+console.log(friends)
 
 // --------------------------------------
 // Exercise 3 - Get the index of first occurance of that value. 
@@ -22,7 +37,7 @@ const significantMathNumbers = [0, 2.718, 3.14159, 1729];
 
 // You want to programmatically find where the number 1729 is in the array.
 // programmatically means that no finger counting allowed. There is a method for this (finding index based of value). 
-
+console.log(`Index of the number 1729: ${significantMathNumbers.indexOf(1729)}`)
 
 // --------------------------------------
 // Exercise 4 - Inserting elements
@@ -30,7 +45,8 @@ const significantMathNumbers = [0, 2.718, 3.14159, 1729];
 const diet = ["tomato", "cucumber", "rocket"];
 
 // You are a programmer. In one line (one statement) insert hamburger, soda and pizza between the elements cucumber and rocket
-
+diet.splice(2, 0, "hamburger", "soda", "pizza")
+console.log(diet)
 
 
 
@@ -39,16 +55,19 @@ const diet = ["tomato", "cucumber", "rocket"];
 
 // Remove the LAST element of the array.
 // Don't remove by index. You know in advance that it's the last in the array because you are too full already. 
-
-
-
+diet.pop()
+console.log(diet)
 
 
 // --------------------------------------
 // Exercise 6 - Copy array
 
-// You really like your daily diet from last exercise. Copy it to a new array called dinnerTray so you can give it to a friend.  
+// You really like your daily diet from last exercise. Copy it to a new array called dinnerTray so you can give it to a friend. 
 
+const dinnerTray = diet.slice()
+//The reason for using .slice() method in this instance, is because if I just made dinnerTray = diet, it'd reference the same
+//location in the memory. Thus, if I add or remove to the diet array, everything would be done to the dinnerTray array aswell.
+console.log(dinnerTray)
 
 
 // --------------------------------------
@@ -57,7 +76,9 @@ const diet = ["tomato", "cucumber", "rocket"];
 const lettersExpanded = ["a","b","c", "d", "e", "f", "g", "h"];
 
 // log every second char in the array starting from b
-
+for (let k = 1; k < lettersExpanded.length; k = k + 2) {
+    console.log(lettersExpanded[k])
+}
 
 
 // --------------------------------------
@@ -69,6 +90,17 @@ const discardedNumbers = [];
 
 // log the element if the number is above 6 or below 0
 // else push them to the array discardedNumbers
+
+for (let m = 0; m < numbers.length; m++) {
+    if (numbers[m] > 6 || numbers[m] < 0) {
+        console.log(numbers[m])
+    }
+    else {
+        discardedNumbers.push(numbers[m])
+    }
+}
+
+console.log(discardedNumbers)
 
 // --------------------------------------
 
